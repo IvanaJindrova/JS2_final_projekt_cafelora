09db71969dc2f472e0a2e92b75ce226dca52a21c
Ivana Jindrová <ivajindrova@gmail.com>
2024-06-28 19:22:13 +0200

    Hamburger menu

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,6 +8,25 @@ import { Gallery } from '../components/Gallery/gallery';
 import { Contact } from '../components/Contact/contact';
 import { Footer } from '../components/Footer/footer';
 
+
+
+document.addEventListener('click', () => {
+  const navBtn = document.querySelector('.nav-btn')
+  const rolloutNav = document.querySelector('.rollout-nav')
+      
+    navBtn.addEventListener('click', () => {
+      if (rolloutNav.classList.contains('nav-closed')) {
+          rolloutNav.classList.remove('nav-closed')
+      } else {
+          rolloutNav.classList.add('nav-closed')
+      }
+  })
+    rolloutNav.addEventListener('click', () => {
+        rolloutNav.classList.add('nav-closed')
+      })
+  })
+
+
 document.querySelector('#root').innerHTML = render(
   <div className="page">
     <Header />
