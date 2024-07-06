a2c21f5ca6130d09ca18f2ea8fd33f42e3442338
Ivana Jindrová <ivajindrova@gmail.com>
2024-07-06 14:26:30 +0200

    Smazání zakomentovaných částí v Drink a Layers + doplnění zatím zakomentovaného kódu na Header

--- src/components/Drink/index.jsx
+++ src/components/Drink/index.jsx
@@ -1,38 +1,5 @@
 import { Layers } from '../Layers/layers';
 import './style.css';
-/*
-export const Drink = ({id, name, ordered, image, layers}) => {
-    return (
-        <>
-            <div className="drink" id={`drinks-${id}`}>
-              <div className="drink__product">
-                <div className="drink__cup">
-                  <img src={image} alt={name}  />
-                </div>
-                <div className="drink__info">
-                  <h3>{name}</h3>
-                  <Layers />
-
-                </div>
-              </div>              
-                <form className="drink__controls">
-                  <input type="hidden" className="order-id" value="0" />
-                  <button className="order-btn">
-                    Objednat
-                  </button>
-              </form>
-              </div>
-        </>
-    )
-}
-
-
-const dataDrink = {
-  name: "Ivanky pití",
-  image: "/favicon.ico"
-}
-*/
-
 
 export const Drink = ({id, name, image, ordered, layers}) => {
     return (
--- src/components/Layers/layers.jsx
+++ src/components/Layers/layers.jsx
@@ -1,22 +1,4 @@
 import './layers.css';
-/*
-export const Layers = (color, label) => {
-    return (
-        <>
-            <div className="layer">
-                <div className="layer__color" style={{ backgroundColor: color }}></div>
-                <div className="layer__label">{label}</div>
-            </div>
-        </>
-    )
-}
-
-
-const dataLayer = {
-    color: "#feeeca",
-    label: "mléčná pěna",
-  }
-*/
 
 export const Layers = ({color, label}) => {
     return (
